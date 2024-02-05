c99f7fc7276bb0465864bdad5d1d5a9ab368814d
ArceaA <81395306+ArceaA@users.noreply.github.com>
2024-02-05 18:25:56 -0500

    "hola"

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const esPrimo= primo=>{
+    let cantDivisores =0
+    for (let i=1; i<=primo; i++){
+        if (primo%i==0) {
+            cantDivisores++
+        }
+    }
+    if (cantDivisores==2) {
+        return true
+    }
+    return false
+}
+const cantidad =15
+let i=0
+let primoTemp=1
+
+while(i<cantidad){
+if (esPrimo(primoTemp)) {
+    console.log(i+1,"->",primoTemp)
+    i++
+}
+primoTemp++
+}
